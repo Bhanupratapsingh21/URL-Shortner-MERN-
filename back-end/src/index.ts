b3dd0e6ab7017800fb47e68b96afc3daddda5ce6
bhanupratapsingh21 <bhanupss137@gmail.com>
2025-04-20 14:16:30 +0530

    added domain or cors

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -33,7 +33,7 @@ app.set('trust proxy', true);
 
 // Middleware setup
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(["*bpss.tech","*vercel"], cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
